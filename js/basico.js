2c8c33fb3a0b8893121b75188c63d8861f1639d8
virtorres <virtorresa@gmail.com>
2020-05-11 21:35:57 +0200

    mi primer commiy

---
+++ js/basico.js
@@ -0,0 +1,13 @@
+// Mi primer archivo js
+
+/* 
+Proyecto "Hola, mundo"
+------------------------
+Subir a GitHub
+*/
+
+// Función declarativa
+function nuevaFuncion() {
+    return 'Hola, mundo';
+}
+
